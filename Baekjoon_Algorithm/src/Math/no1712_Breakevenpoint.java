4af394d03cb149ae3510b18c0fef0d70efb1431e
seungEH <48157056+seungEH@users.noreply.github.com>
2019-12-21 15:03:29 +0900

    Update no1712_Breakevenpoint.java

--- Baekjoon_Algorithm/src/Math/no1712_Breakevenpoint.java
+++ Baekjoon_Algorithm/src/Math/no1712_Breakevenpoint.java
@@ -1,7 +1,23 @@
 package Math;
 
+import java.util.Scanner;
+
 public class no1712_Breakevenpoint {
 	
+	public static void main(String[] args) {
+		
+	 Scanner sc = new Scanner(System.in);
 	
-
+	 int fix_cost = sc.nextInt();
+	 int var_cost = sc.nextInt();
+	 int sell_price = sc.nextInt();
+	 int index = 0;
+	
+	 if(var_cost >= sell_price) {
+		 System.out.println(-1);
+	 }else {
+		 index = fix_cost/(sell_price-var_cost);
+		 System.out.println(index+1);
+	 }
+	}
 }
