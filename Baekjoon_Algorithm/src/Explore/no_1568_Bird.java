4da1ad6e3a99d5317324240469ee00acb03ef667
seungEH <48157056+seungEH@users.noreply.github.com>
2020-02-11 23:34:12 +0900

    Update no_1568_Bird.java

--- Baekjoon_Algorithm/src/Explore/no_1568_Bird.java
+++ Baekjoon_Algorithm/src/Explore/no_1568_Bird.java
@@ -12,21 +12,30 @@ public class no_1568_Bird {
 		
 		while(N>=1) {
 			if(bird>N) {
-				System.out.println("if");
-				System.out.println(bird);
 				bird =1;
 			} else {
-				System.out.println("else");
-				System.out.println("N= "+N);
-				System.out.println("bird=" +bird);
 				N -= bird;
 				bird++;
 				count++;
 			}
 		}
 		System.out.println(count);
-		
-
 	}
-
 }
+
+//14 -> 7
+
+//1마리가 날아가면 13마리가 남는다.
+//
+//2마리가 날아가면 11마리가 남는다.
+//
+//3마리가 날아가면 8마리가 남는다.
+//
+//4마리가 날아가면 4마리가 남는다.
+//
+//1마리가 날아가면 3마리가 남는다. --> 여기서 조건이 걸리게 되므로 다시 날아가야할 마리수가 1로 초기화함.
+//
+//2마리가 날아가면 1마리가 남는다. 
+//
+//1마리가 날아가면 0마리가 남는다. 
+
